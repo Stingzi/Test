7fdf3771a75026ef9379a33841fb32b76d2eef5e
Stingzi <votinglaotu@gmail.com>
2025-02-21 17:07:24 +0700

    Refactor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const morgan = require('morgan');
-const hbs = require('express-handlebars')
-const path = require('path');
-const app = express();
-const port = 3000;
+    const express = require('express');
+    const morgan = require('morgan');
+    const hbs = require('express-handlebars')
+    const path = require('path');
+    const app = express();
+    const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public'))); // Đường dẫn đến thư mục public
+    const route = require('./routes')
 
-//morgan HTTP logger
-app.use(morgan('combined'));
+    app.use(express.static(path.join(__dirname, 'public'))); // Đường dẫn đến thư mục public
 
-// Template engine
-app.engine('hbs', hbs.engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
-    // layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
-    // partialsDir: path.join(__dirname, 'resources', 'views', 'partials'), // Đường dẫn đến partials
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-console.log("Views Path:", path.join(__dirname, 'resources', 'views'));
-console.log("Partials Path:", path.join(__dirname, 'resources', 'views', 'partials'));
+    //morgan HTTP logger
+    // app.use(morgan('combined'));
 
-//route
-app.get('/', (req, res) => {
-    res.render('home');
-})
+    // Template engine
+    app.engine('hbs', hbs.engine({
+        extname: 'hbs',
+        defaultLayout: 'main',
+        // layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+        // partialsDir: path.join(__dirname, 'resources', 'views', 'partials'), // Đường dẫn đến partials
+    }));
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+    // console.log("Views Path:", path.join(__dirname, 'resources', 'views'));
+    // console.log("Partials Path:", path.join(__dirname, 'resources', 'views', 'partials'));
 
-// 127.0.0.1 - localhost
+    // Route init
+    route(app);
 
-app.listen(port,() =>{
-    console.log(`Server is running on port ${port}`)
-})
+  
+
+    // 127.0.0.1 - localhost
+
+    app.listen(port,() =>{
+        console.log(`Server is running on port ${port}`)
+    })
